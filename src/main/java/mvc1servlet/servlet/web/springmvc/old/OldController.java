eaea5b41236c2bd6933ec2b906ced9415a7ba280
jinhojava <ho042479@gmail.com>
2024-01-19 15:02:53 +0900

    Second commit

--- src/main/java/mvc1servlet/servlet/web/springmvc/old/OldController.java
+++ src/main/java/mvc1servlet/servlet/web/springmvc/old/OldController.java
@@ -14,3 +14,7 @@ public class OldController implements Controller {
         return new ModelAndView("new-form");
     }
 }
+
+
+//핸들러매핑에서 컨트롤러를 찾을 수 있어야한다. //스프링빈의 이름으로 핸들러를 찾을 수 있는 핸들러 매핑이 필요한 것이다.
+//핸들러 매핑을 통해서 찾은 핸들러를 실행할 수 있는 핸들러 어댑터가 필요하다.
